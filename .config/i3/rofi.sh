1c16199f4587844fb6258a1f178e864581a82f2e
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2020-02-14 18:51:37 +0100

    Some changes in compton, rofi and settings.json

--- .config/i3/rofi.sh
+++ .config/i3/rofi.sh
@@ -4,4 +4,4 @@ bg_color=#2f343f
 text_color=#f3f4f5
 htext_color=#72ada7
 
-rofi -show run -lines 5 -eh 2 -width 100 -padding 675 -bw 40 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "System San Francisco Display 18"
+rofi -show run -lines 5 -eh 2 -width 100 -padding 355 -bw 40 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "System San Francisco Display 18"
