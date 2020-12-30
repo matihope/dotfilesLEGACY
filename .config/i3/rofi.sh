d752273b6741928b24e5ca7572fa2808150f425b
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2020-12-30 17:09:05 +0100

    Reverting to c243f5c plus some new changes

---
+++ .config/i3/rofi.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+bg_color=#2f343f
+text_color=#f3f4f5
+htext_color=#72ada7
+
+rofi -show run -lines 5 -eh 2 -width 100 -padding 355 -bw 40 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "System San Francisco Display 18"
---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar -c ~/.config/polybar/config.ini top &
+# exec hideIt.sh --name '^polybar-top_HDMI-0$' --region 0x0+1080+-1 --direction top -w -i 0.5 &
