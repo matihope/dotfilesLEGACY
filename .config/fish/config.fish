ce3f6b5a58d991d31f15dd93d8f6c4e0d5295649
Mateusz Kołpa <mateusz.kolpa@wp.pl>
2021-03-09 17:43:35 +0100

    Update 09.03.2021 Desktop

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+set theme_color_scheme nord
+set EDITOR "nvim"
+set theme_nerd_fonts yes
+set theme_display_git yes
+set fish_prompt_pwd_dir_length 0
+alias q="exit"
+
+abbr p "sudo pacman"
+abbr n "nvim"
+abbr sn "sudo nvim"
+abbr ss "sudo systemctl"
+
